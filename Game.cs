13f3e591794fa48c42618846b7c5ac87743e7bdd
Pavel Shemelin <pauls0@mail.ru>
2018-12-05 19:34:32 +0300

    ДЗ №1

---
+++ Game.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace LeaningCSharp2_Lesson1
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static BaseObject[] _objs;
+
+        private static Random rand;
+
+        static Game()
+        {
+            rand = new Random( 0 );
+        }
+        public static void Init( Form form )
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            // Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate( g, new Rectangle( 0, 0, Width, Height ) );
+
+            //загрузка 
+            Load();
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick( object sender, EventArgs e )
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            // Проверяем вывод графики
+            //Buffer.Graphics.Clear( Color.Black );
+            //Buffer.Graphics.DrawRectangle( Pens.White, new Rectangle( 100, 100, 200, 200 ) );
+            //Buffer.Graphics.FillEllipse( Brushes.Wheat, new Rectangle( 100, 100, 200, 200 ) );
+            //Buffer.Render();
+
+            Buffer.Graphics.Clear( Color.Black );
+            foreach (BaseObject obj in _objs) obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[60];
+
+            //летающая тарелка
+            //запускаем сначала ее, чтобы астероиды все двигались
+            _objs[0] = new Ufo( new Point( 600, rand.Next( Height ) ), new Point( -8, 0 ), new Size( 40, 40 ) );
+
+            //астероиды
+            for (int i = 1; i <= 15; i++)
+            {
+                _objs[i] = new Asteroid( new Point( 600, i * 20 ), new Point( -i, -i * 2 ), new Size( 20, 20 ) );
+            }
+
+            //звезды
+            for (int i = 16; i <= 35; i++)
+            {
+                _objs[i] = new Star( new Point( 600,  rand.Next( 0, Height ) ), new Point( i, 0 ), new Size( 15, 15 ) );
+            }
+
+            //звездная пыль
+            for (int i = 36; i < _objs.Length; i++)
+            {
+                _objs[i] = new StarDust( new Point( 600, rand.Next( 0, Height ) ), new Point( i, 0 ), new Size( 3, 3 ) );
+            }
+        }
+    }
+}
---
+++ Objects.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace LeaningCSharp2_Lesson1
+{
+    abstract class BaseObject
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+        static BaseObject(){}
+
+        public BaseObject( Point pos, Point dir, Size size )
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse( Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height );
+        }
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+    }
+
+    class Star : BaseObject
+    {
+        private Image image;
+
+        public Star( Point pos, Point dir, Size size ) : base( pos, dir, size )
+        {
+            image = new Bitmap(Assembly.GetEntryAssembly().GetManifestResourceStream( "LeaningCSharp2_Lesson1.Resources.Star.png" ) );
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage( image, new Rectangle( this.Pos, this.Size ) );
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+        }
+    }
+
+    class Asteroid : BaseObject
+    {
+        static private Image[] imageColl;
+        static private Random rand;
+        private Image image;
+
+        static Asteroid()
+        {
+            imageColl = new Image[2];
+            imageColl[0] = new Bitmap( Assembly.GetEntryAssembly().GetManifestResourceStream( "LeaningCSharp2_Lesson1.Resources.Asteroid1.png" ) );
+            imageColl[1] = new Bitmap( Assembly.GetEntryAssembly().GetManifestResourceStream( "LeaningCSharp2_Lesson1.Resources.Asteroid2.png" ) );
+            rand = new Random(0);
+        }
+
+        public Asteroid( Point pos, Point dir, Size size ) : base( pos, dir, size )
+        {
+            image = imageColl[rand.Next(0, 2)];
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage( image, new Rectangle( this.Pos, this.Size ) );
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+
+    class Ufo : BaseObject
+    {
+        private static Image image;
+        private static Random rand;
+
+        static Ufo()
+        {
+            image = new Bitmap( Assembly.GetEntryAssembly().GetManifestResourceStream( "LeaningCSharp2_Lesson1.Resources.UFO.png" ) );
+            rand = new Random();
+        }
+
+        public Ufo( Point pos, Point dir, Size size ) : base( pos, dir, size )
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage( image, new Rectangle( this.Pos, this.Size ) );
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X > Game.Width)
+            {
+                Pos.X = 0;
+                Pos.Y = rand.Next(Game.Height);
+            }
+        }
+    }
+
+    class StarDust : BaseObject
+    {
+        public StarDust( Point pos, Point dir, Size size ) : base( pos, dir, size )
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawLine( Pens.White, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height );
+            Game.Buffer.Graphics.DrawLine( Pens.White, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height );
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+        }
+    }
+}
